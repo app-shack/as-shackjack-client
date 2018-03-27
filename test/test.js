ed56d99518099b7be760a5f66e84f892ef90d3a9
Pontus Andersson <pontus.andersson90@gmail.com>
2018-03-27 15:21:14 +0200

    initial project structure

---
+++ test/test.js
@@ -0,0 +1,10 @@
+let chai = require('chai');
+let assert = chai.assert;
+
+describe('Array', function() {
+  it('should start empty', function() {
+    var arr = [];
+
+    assert.equal(arr.length, 0);
+  });
+});
