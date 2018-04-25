e165337d45fcfee28dc8ca2dd6c0d2bc2816ae67
Pontus Andersson <pontus.andersson90@gmail.com>
2018-04-25 09:32:22 +0200

    Bets are placed as numbers (not integers) Fix lint errors Document functions with placeholders

--- src/socket.js
+++ src/socket.js
@@ -4,7 +4,7 @@ const { Player } = require("./models/player");
 class Socket {
 	
 	constructor(url, bot) {
-		this.bot = bot
+		this.bot = bot;
 		this.socket = io.connect(url, { reconnect: true, query: "teamName=" + this.bot.teamName });
 		this.configureListeners();
 	}
@@ -17,31 +17,27 @@ class Socket {
 			});
 
 			this.socket.on("requestAction", function(data) {
-				this.onRequestAction(data)
+				this.onRequestAction(data);
 			}.bind(this));
 
 			this.socket.on("requestBet", function(data) {
-				this.onRequestBet(data)
+				this.onRequestBet(data);
 			}.bind(this));
 		}.bind(this));
 	}
 
-	
-
 	onRequestBet(state) {
-
 		let you = new Player(state.players.filter(player => {
-				return player.teamName === this.bot.teamName
-			})[0]);
+			return player.teamName === this.bot.teamName;
+		})[0]);
 		let others = state.players.filter(player => player.teamName != this.bot.teamName).map(data => new Player(data));
 
 		this.sendData("bet", {amount: this.bot.bet(you,  others)});
 	}
 
 	onRequestAction(state) {
-
 		let you = new Player(state.players.filter(player => {
-			return player.teamName === this.bot.teamName
+			return player.teamName === this.bot.teamName;
 		})[0]);
 		let others = state.players.filter(player => player.teamName != this.bot.teamName).map(data => new Player(data));
 		let dealer = state.dealersHand;
@@ -49,12 +45,11 @@ class Socket {
 		this.sendData("action", {action: this.bot.action(you, dealer, others)});
 	}
 
+	// Actions
 
 	sendData(event, data) {
 		this.socket.emit(event, data);
 	}
-
-	// Actions
 
 	sendAction(action) {
 		let data = {
